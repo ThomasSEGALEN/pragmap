0e9e9a41b7d66e8de431dc63e72125656722273d
Jean-Marin Monnier <73218544+Jean-MarinMonnier@users.noreply.github.com>
2023-12-22 23:06:42 +0100

    Création d'un Dockerfile pour la conteneurisation de l'API

--- Back/Pragmap/Pragmap.API/Program.cs
+++ Back/Pragmap/Pragmap.API/Program.cs
@@ -25,10 +25,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
+}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
