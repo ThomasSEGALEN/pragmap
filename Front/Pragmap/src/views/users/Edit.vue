c75a15054420106dd8367a944b0b1daf29f1271e
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-01-12 18:48:14 +0100

    Création des vues du CRUD Users

---
+++ Front/Pragmap/src/views/users/Edit.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Layout } from '@/components/layouts'
+
+defineProps({
+  id: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <Layout>
+    <div>Users edit {{ id }}</div>
+  </Layout>
+</template>
