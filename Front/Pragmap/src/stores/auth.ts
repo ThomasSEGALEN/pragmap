ed826b97ef908adb892f84f7f5f0d38c60f9c07c
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-01-13 23:57:21 +0100

    Modifications du authStore, import de l'objet User

--- Front/Pragmap/src/stores/auth.ts
+++ Front/Pragmap/src/stores/auth.ts
@@ -1,38 +1,42 @@
 import { defineStore } from 'pinia'
 import { useLocalStorage } from '@vueuse/core'
 import { api } from '@/main'
+import type { IUser } from '@/types'
+import { jwtDecode } from "jwt-decode"
 
 export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    accessToken: useLocalStorage('accessToken', ''),
-    refreshToken: useLocalStorage('refreshToken', '')
-  }),
-  getters: {
-    isAuthenticated: (state) => !!state.accessToken
-  },
-  actions: {
-    async login(email: string, password: string) {
-      const response = await api.post('/auth/login', {
-        email: email,
-        password: password
-      })
-      const { accessToken, refreshToken } = response.data
+	state: () => ({
+		user: useLocalStorage<Omit<IUser, 'password'>>('user', null),
+		accessToken: useLocalStorage('accessToken', '')
+	}),
+	getters: {
+		isAuthenticated: (state) => !!state.accessToken
+	},
+	actions: {
+		async login(email: string, password: string) {
+			const response = await api.post('/auth/login', {
+				email: email,
+				password: password
+			})
+			const { accessToken } = response.data
 
-      this.setToken(accessToken, refreshToken)
-    },
-    logout() {
-      this.clearToken()
-    },
-    getToken(token: string) {
-      return this.$state[token as keyof typeof this.$state]
-    },
-    setToken(accessToken: string, refreshToken: string) {
-      this.accessToken = accessToken
-      this.refreshToken = refreshToken
-    },
-    clearToken() {
-      this.accessToken = ''
-      this.refreshToken = ''
-    }
-  }
+			this.accessToken = accessToken
+
+			await this.getUserById(jwtDecode<{ nameid: string; }>(accessToken).nameid)
+		},
+		logout() {
+			this.clearToken()
+		},
+		async getUserById(id: string) {
+			const response = await api.get(`/user/${id}`)
+
+			this.user = response.data.user
+		},
+		getToken(token: string) {
+			return this.$state[token as keyof typeof this.$state]
+		},
+		clearToken() {
+			this.accessToken = ''
+		}
+	}
 })
