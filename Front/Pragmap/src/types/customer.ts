db70ca4b13619b98015ecf755bc3d18adf69a755
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-01-13 23:57:40 +0100

    Création des types client, utilisateur et rôle

---
+++ Front/Pragmap/src/types/customer.ts
@@ -0,0 +1,9 @@
+export interface ICustomer {
+	id: string;
+	name: string;
+	logo: string;
+	createdAt: string;
+	updatedAt: string;
+	managerId: string;
+	authorizationId: string[];
+}
---
+++ Front/Pragmap/src/types/index.ts
@@ -0,0 +1,3 @@
+export * from './customer'
+export * from './user'
+export * from './role'
