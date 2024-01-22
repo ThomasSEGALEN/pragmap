c509c5e2effc6f187c3916f596b458b046e3e6cb
Jean-Marin Monnier <73218544+Jean-MarinMonnier@users.noreply.github.com>
2024-01-22 12:02:59 +0100

    implémentation du refreshtoken

---
+++ Back/Pragmap/Pragmap.API/Application/Commands/Auth/RefreshTokenCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Pragmap.API.Application.DTOs;
+using Pragmap.API.Application.Models;
+
+namespace Pragmap.API.Application.Commands.Auth
+{
+    public class RefreshTokenCommand : IRequest<CommandResult<string>>
+    {
+        public string RefreshToken { get; set; }
+    }
+}
