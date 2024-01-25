7b2cfb4365d0e047005a8b4b5a67fc868070606e
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2024-01-25 01:35:31 +0100

    Update UpdateUserCommand.cs

--- Back/Pragmap/Pragmap.API/Application/Commands/User/UpdateUserCommand.cs
+++ Back/Pragmap/Pragmap.API/Application/Commands/User/UpdateUserCommand.cs
@@ -8,8 +8,9 @@ namespace Pragmap.API.Application.Commands
     public class UpdateUserCommand : IRequest<CommandResult<User>>
     {
         public Guid Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public Guid? RoleId { get; set; }    
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid RoleId { get; set; }    
     }
 }
