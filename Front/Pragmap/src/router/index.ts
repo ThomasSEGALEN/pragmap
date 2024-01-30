37b77ad9d93aae10f1c224cc79a1c9ed0c424c3b
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-01-30 03:52:04 +0100

    Formattage de fichiers

--- Front/Pragmap/src/router/index.ts
+++ Front/Pragmap/src/router/index.ts
@@ -3,98 +3,98 @@ import { useAuthStore, useUserStore } from '@/stores'
 import authMiddleware from '@/middlewares/auth'
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: '/users',
-			name: 'UsersIndex',
-			component: () => import('@/views/users/Index.vue'),
-			beforeEnter: authMiddleware,
-			meta: { requiresAuth: true }
-		},
-		{
-			path: '/users/create',
-			name: 'UsersCreate',
-			component: () => import('@/views/users/Create.vue'),
-			beforeEnter: authMiddleware,
-			meta: { requiresAuth: true }
-		},
-		{
-			path: '/users/:id/edit',
-			name: 'UsersEdit',
-			component: () => import('@/views/users/Edit.vue'),
-			beforeEnter: (to) => {
-				authMiddleware
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/users',
+      name: 'UsersIndex',
+      component: () => import('@/views/users/Index.vue'),
+      beforeEnter: authMiddleware,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/users/create',
+      name: 'UsersCreate',
+      component: () => import('@/views/users/Create.vue'),
+      beforeEnter: authMiddleware,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/users/:id/edit',
+      name: 'UsersEdit',
+      component: () => import('@/views/users/Edit.vue'),
+      beforeEnter: (to) => {
+        authMiddleware
 
-				const { getEditUserById } = useUserStore()
+        const { getEditUserById } = useUserStore()
 
-				try {
-					getEditUserById(to.params.id.toString())
-				} catch (error) {
-					return { name: 'Home' }
-				}
-			},
-			meta: { requiresAuth: true },
-			props: true
-		},
-		{
-			path: '/',
-			name: 'Home',
-			component: () => import('@/views/Home.vue'),
-			beforeEnter: authMiddleware,
-			meta: { requiresAuth: true }
-		},
-		{
-			path: '/about',
-			name: 'About',
-			component: () => import('@/views/About.vue'),
-			beforeEnter: authMiddleware,
-			meta: { requiresAuth: true }
-		},
-		{
-			path: '/logout',
-			name: 'Logout',
-			redirect: () => {
-				const { logout } = useAuthStore()
+        try {
+          getEditUserById(to.params.id.toString())
+        } catch (error) {
+          return { name: 'Home' }
+        }
+      },
+      meta: { requiresAuth: true },
+      props: true
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/views/Home.vue'),
+      beforeEnter: authMiddleware,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: () => import('@/views/About.vue'),
+      beforeEnter: authMiddleware,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/logout',
+      name: 'Logout',
+      redirect: () => {
+        const { logout } = useAuthStore()
 
-				logout()
+        logout()
 
-				return { name: 'Login' }
-			},
-			beforeEnter: authMiddleware,
-			meta: { requiresAuth: true }
-		},
-		{
-			path: '/login',
-			name: 'Login',
-			component: () => import('@/views/Login.vue')
-		},
-		{
-			path: '/forgot-password',
-			name: 'ForgotPassword',
-			component: () => import('@/views/ForgotPassword.vue')
-		},
-		{
-			path: '/reset-password',
-			name: 'ResetPassword',
-			component: () => import('@/views/ResetPassword.vue')
-		},
-		{
-			path: '/:pathMatch(.*)*',
-			name: 'NotFound',
-			component: () => import('@/views/NotFound.vue')
-		}
-	]
+        return { name: 'Login' }
+      },
+      beforeEnter: authMiddleware,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/views/Login.vue')
+    },
+    {
+      path: '/forgot-password',
+      name: 'ForgotPassword',
+      component: () => import('@/views/ForgotPassword.vue')
+    },
+    {
+      path: '/reset-password',
+      name: 'ResetPassword',
+      component: () => import('@/views/ResetPassword.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('@/views/NotFound.vue')
+    }
+  ]
 })
 
 router.beforeEach((to, from, next) => {
-	const { isAuthenticated } = useAuthStore()
-	const requiresAuth = to.meta.requiresAuth
+  const { isAuthenticated } = useAuthStore()
+  const requiresAuth = to.meta.requiresAuth
 
-	if (!isAuthenticated && requiresAuth) return next({ name: 'Login' })
-	if (isAuthenticated && !requiresAuth) return next({ name: 'Home' })
+  if (!isAuthenticated && requiresAuth) return next({ name: 'Login' })
+  if (isAuthenticated && !requiresAuth) return next({ name: 'Home' })
 
-	return next()
+  return next()
 })
 
 export default router
