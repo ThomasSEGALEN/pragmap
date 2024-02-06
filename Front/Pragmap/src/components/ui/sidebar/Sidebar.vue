89b36d30d2333751fe1d5a939d055d313fc9f8fd
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-06 23:43:31 +0100

    Création du composant Sidebar

--- Front/Pragmap/src/components/ui/sidebar/Sidebar.vue
+++ Front/Pragmap/src/components/ui/sidebar/Sidebar.vue
@@ -1,64 +1,57 @@
 <script setup lang="ts">
-import { useColorMode, useLocalStorage } from '@vueuse/core'
+import { useLocalStorage } from '@vueuse/core'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
-import { ChevronDown, ChevronUp } from 'lucide-vue-next'
+import {
+	ChevronDown,
+	ChevronUp,
+	FolderKanban,
+	Handshake,
+	LogOut,
+	UserRound,
+	UserRoundCog
+} from 'lucide-vue-next'
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
-import { Moon, Sun } from 'lucide-vue-next'
 import { RouterLink } from 'vue-router'
+import { Separator } from '@/components/ui/separator'
+import { useAuthStore } from '@/stores'
 
+const { user } = useAuthStore()
 const isUserOpen = useLocalStorage('isUserOpen', true)
 const isCustomerOpen = useLocalStorage('isCustomerOpen', true)
-const mode = useColorMode()
-const switchMode = () => {
-	mode.value = mode.value === 'light' ? 'dark' : 'light'
-}
+const isRoadmapOpen = useLocalStorage('isRoadmapOpen', true)
 </script>
 
 <template>
 	<div :class="cn('pb-12', $attrs.class ?? '')">
-		<div class="p-4">
-			<div class="flex items-center justify-between mb-10">
-				<RouterLink to="/">
-					<h2 class="text-lg font-semibold tracking-tight">Pragmap</h2>
-				</RouterLink>
-				<Button
-					class=""
-					variant="outline"
-					size="icon"
-					@click="switchMode"
-				>
-					<Sun
-						v-if="mode === 'light'"
-						class="w-4 h-4"
-					/>
-					<Moon
-						v-if="mode === 'dark'"
-						class="w-4 h-4"
-					/>
-				</Button>
-			</div>
-			<div class="space-y-4">
-				<div>
+		<div class="h-full p-2">
+			<RouterLink to="/">
+				<h2 class="text-2xl font-semibold leading-none tracking-tighter px-4 py-2">Pragmap</h2>
+			</RouterLink>
+			<div class="h-full flex flex-col justify-between">
+				<div class="space-y-4 py-10 px-4">
 					<Collapsible v-model:open="isUserOpen">
-						<CollapsibleTrigger class="flex justify-between items-center">
-							<h2 class="text-lg font-semibold tracking-tight mr-2">Utilisateurs</h2>
+						<CollapsibleTrigger class="flex justify-between items-center mb-2">
+							<div class="flex items-center font-semibold text-sm mr-2">
+								<UserRound class="h-4 w-4 mr-2" />
+								Utilisateurs
+							</div>
 							<ChevronDown
 								v-if="!isUserOpen"
-								class="w-4 h-4"
+								class="h-4 w-4"
 							/>
 							<ChevronUp
 								v-else
-								class="w-4 h-4"
+								class="h-4 w-4"
 							/>
 						</CollapsibleTrigger>
-						<CollapsibleContent>
+						<CollapsibleContent class="flex flex-col space-y-2">
 							<RouterLink to="/users">
 								<Button
 									variant="ghost"
 									class="h-8 w-full justify-start"
 								>
-									Liste
+									Lister
 								</Button>
 							</RouterLink>
 							<RouterLink to="/users/create">
@@ -66,32 +59,33 @@ const switchMode = () => {
 									variant="ghost"
 									class="h-8 w-full justify-start"
 								>
-									Création
+									Créer
 								</Button>
 							</RouterLink>
 						</CollapsibleContent>
 					</Collapsible>
-				</div>
-				<div>
 					<Collapsible v-model:open="isCustomerOpen">
-						<CollapsibleTrigger class="flex justify-between items-center">
-							<h2 class="text-lg font-semibold tracking-tight mr-2">Clients</h2>
+						<CollapsibleTrigger class="flex justify-between items-center mb-2">
+							<div class="flex items-center font-semibold text-sm mr-2">
+								<Handshake class="h-4 w-4 mr-2" />
+								Clients
+							</div>
 							<ChevronDown
 								v-if="!isCustomerOpen"
-								class="w-4 h-4"
+								class="h-4 w-4"
 							/>
 							<ChevronUp
 								v-else
-								class="w-4 h-4"
+								class="h-4 w-4"
 							/>
 						</CollapsibleTrigger>
-						<CollapsibleContent>
+						<CollapsibleContent class="flex flex-col space-y-2">
 							<RouterLink to="/customers">
 								<Button
 									variant="ghost"
 									class="h-8 w-full justify-start"
 								>
-									Liste
+									Lister
 								</Button>
 							</RouterLink>
 							<RouterLink to="/customers/create">
@@ -99,11 +93,80 @@ const switchMode = () => {
 									variant="ghost"
 									class="h-8 w-full justify-start"
 								>
-									Création
+									Créer
 								</Button>
 							</RouterLink>
 						</CollapsibleContent>
 					</Collapsible>
+					<Collapsible v-model:open="isRoadmapOpen">
+						<CollapsibleTrigger class="flex justify-between items-center mb-2">
+							<div class="flex items-center font-semibold text-sm mr-2">
+								<FolderKanban class="h-4 w-4 mr-2" />
+								Roadmaps
+							</div>
+							<ChevronDown
+								v-if="!isRoadmapOpen"
+								class="h-4 w-4"
+							/>
+							<ChevronUp
+								v-else
+								class="h-4 w-4"
+							/>
+						</CollapsibleTrigger>
+						<CollapsibleContent class="flex flex-col space-y-2">
+							<RouterLink to="/roadmaps">
+								<Button
+									variant="ghost"
+									class="h-8 w-full justify-start"
+								>
+									Lister
+								</Button>
+							</RouterLink>
+							<RouterLink to="/roadmaps/create">
+								<Button
+									variant="ghost"
+									class="h-8 w-full justify-start"
+								>
+									Créer
+								</Button>
+							</RouterLink>
+						</CollapsibleContent>
+					</Collapsible>
+				</div>
+				<div>
+					<div class="flex flex-col space-y-2 px-2">
+						<p class="font-medium leading-none break-words">
+							{{ user.firstName }} {{ user.lastName }}
+						</p>
+						<p class="text-sm leading-none text-muted-foreground break-words">
+							{{ user.email }}
+						</p>
+					</div>
+					<Separator class="my-4" />
+					<div class="flex flex-col space-y-2">
+						<RouterLink to="/profile">
+							<Button
+								variant="ghost"
+								class="h-8 w-full justify-start"
+							>
+								<div class="flex items-center font-semibold">
+									<UserRoundCog class="h-4 w-4 mr-2" />
+									Profil
+								</div>
+							</Button>
+						</RouterLink>
+						<RouterLink to="/logout">
+							<Button
+								variant="ghost"
+								class="h-8 w-full justify-start"
+							>
+								<div class="flex items-center font-semibold">
+									<LogOut class="h-4 w-4 mr-2" />
+									Déconnexion
+								</div>
+							</Button>
+						</RouterLink>
+					</div>
 				</div>
 			</div>
 		</div>
