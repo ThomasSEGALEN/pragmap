9f01ed74af5628b5bcea2dea1ca9297a7ec986cb
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-06 04:11:41 +0100

    Update base service generics

--- Front/Pragmap/src/services/user.ts
+++ Front/Pragmap/src/services/user.ts
@@ -1,6 +1,6 @@
 import { BaseService } from '@/services'
-import type { IGetUser, IPostUser, IPutUser } from '@/types'
+import type { IGetUser, IPostUser, IPutUser, IUser } from '@/types'
 
-class UserService extends BaseService<IGetUser, IPostUser, IPutUser> {}
+class UserService extends BaseService<IUser, IGetUser, IPostUser, IPutUser> {}
 
 export const userService = new UserService('user')
