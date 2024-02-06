e830e472a90f31f4a857b2bb3e5da0c9f8ca7232
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-06 23:41:50 +0100

    Mise à jour des layouts

--- Front/Pragmap/src/components/layouts/Layout.vue
+++ Front/Pragmap/src/components/layouts/Layout.vue
@@ -1,14 +1,124 @@
 <script setup lang="ts">
+import { useColorMode } from '@vueuse/core'
+import { Avatar } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Menu, X } from 'lucide-vue-next'
 import { Sidebar } from '@/components/ui/sidebar'
 import { Toaster } from '@/components/ui/toast'
+import { useAuthStore } from '@/stores'
+import { ref } from 'vue'
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue
+} from '@/components/ui/select'
+
+const theme = useColorMode()
+const selectTheme = (value: string) => (theme.value = value as 'light' | 'dark')
+const isToggled = ref(false)
+const toggleSidebar = () => (isToggled.value = !isToggled.value)
+const { user } = useAuthStore()
+const getInitials = () => `${user.firstName.charAt(0)}${user.lastName.charAt(0)}`.toUpperCase()
 </script>
 
 <template>
-	<div class="h-full flex">
-		<Sidebar class="w-72 border-r" />
-		<main class="h-fit w-full flex justify-center p-4 md:p-12">
-			<slot />
-		</main>
+	<div class="flex">
+		<Sidebar class="min-h-screen min-w-60 hidden md:block border-r" />
+		<div class="w-full flex flex-col">
+			<header
+				v-if="$slots.header"
+				class="flex justify-between items-center text-xl font-semibold leading-none tracking-tighter p-4 border-b"
+			>
+				<Button
+					class="md:hidden h-8 w-8"
+					variant="ghost"
+					size="icon"
+					@click="toggleSidebar"
+				>
+					<Menu v-if="!isToggled" />
+					<X v-else />
+				</Button>
+				<slot name="header" />
+				<DropdownMenu>
+					<DropdownMenuTrigger as-child>
+						<Button
+							variant="ghost"
+							class="relative h-8 w-8 rounded-full"
+						>
+							<Avatar class="h-8 w-8">
+								{{ getInitials() }}
+							</Avatar>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent
+						class="w-56"
+						align="end"
+					>
+						<DropdownMenuLabel class="font-normal flex">
+							<div class="flex flex-col space-y-1">
+								<p class="text-sm font-medium leading-none break-words">
+									{{ user.firstName }} {{ user.lastName }}
+								</p>
+								<p class="text-xs leading-none text-muted-foreground break-words">
+									{{ user.email }}
+								</p>
+							</div>
+						</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuGroup>
+							<RouterLink to="/profile">
+								<DropdownMenuItem>Profil</DropdownMenuItem>
+							</RouterLink>
+							<DropdownMenuItem as-child>
+								<Select
+									:default-value="theme"
+									@update:model-value="selectTheme"
+								>
+									<SelectTrigger
+										class="h-fit w-full border-none outline-none focus:outline-none ring-0 focus:ring-0 ring-offset-0 focus:ring-offset-0 px-2 py-1.5"
+									>
+										<SelectValue
+											:placeholder="theme === 'light' ? 'Thème clair' : 'Thème sombre'"
+										/>
+									</SelectTrigger>
+									<SelectContent>
+										<SelectGroup>
+											<SelectItem value="light">Thème clair </SelectItem>
+											<SelectItem value="dark">Thème sombre</SelectItem>
+										</SelectGroup>
+									</SelectContent>
+								</Select>
+							</DropdownMenuItem>
+						</DropdownMenuGroup>
+						<DropdownMenuSeparator />
+						<RouterLink to="/logout">
+							<DropdownMenuItem>Déconnexion</DropdownMenuItem>
+						</RouterLink>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</header>
+			<div
+				class="min-h-80 w-full border-b"
+				:class="isToggled ? 'block' : 'hidden'"
+			>
+				TODO: RESPONSIVE SIDEBAR
+			</div>
+			<main class="h-fit w-full flex justify-center p-6 md:p-12">
+				<slot />
+			</main>
+		</div>
 		<Toaster />
 	</div>
 </template>
