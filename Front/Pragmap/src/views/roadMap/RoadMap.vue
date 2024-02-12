087315872c1ba1ff2f4ed082da9e789a6b84fa14
yonjukyu <yonjukyu2@gmail.com>
2024-02-13 00:26:08 +0100

    add new all tryucs bravo a moi bortdel sucez moi bandes 'encu

--- Front/Pragmap/src/views/roadMap/RoadMap.vue
+++ Front/Pragmap/src/views/roadMap/RoadMap.vue
@@ -2,23 +2,39 @@
   <Layout>
     <div>
       <div class="navbar">
-        <button @click="addNode('inputed', 'inputed')">Input</button>
-        <button @click="addNode('outputed','outputed')">Output</button>
+        <button @click="addNode('tache')">Tache</button>
+        <button @click="addNode('jalon')">Jalon</button>
+        <button @click="addNode('livrable')">Livrable</button>
         <button @click="saveNode()">Save</button>
         <button @click="importNode()">Load</button>
       </div>
-      <VueFlow v-model="elements" class="vue-flow-basic-example" :default-zoom="1" :min-zoom="0.2" :max-zoom="4" style="height: 80vh;">
+      <VueFlow v-model="elements" class="vue-flow-basic-example" :default-zoom="1" :min-zoom="0.2" :max-zoom="4"
+        style="height: 80vh;">
         <Background pattern-color="#aaa" :gap="8" />
         <MiniMap />
-        <template #node-inputed="nodeProps">
-          <InputNode v-bind="nodeProps" />
+        <template #node-tache="nodeProps">
+          <TacheNode @node-clicked="selectedNodeId = $event" v-bind="nodeProps" />
         </template>
-        <template #node-outputed="nodeProps">
-          <OutputNode v-bind="nodeProps" />
+        <template #node-jalon="nodeProps">
+          <JalonNode @node-clicked="selectedNodeId = $event" v-bind="nodeProps" />
+        </template>
+        <template #node-livrable="nodeProps">
+          <LivrableNode @node-clicked="selectedNodeId = $event" v-bind="nodeProps" />
         </template>
       </VueFlow>
     </div>
   </Layout>
+  <div v-if="selectedNodeId && selectedNode" class="settingsWindow">
+    <h2>Node Settings</h2>
+    <div v-for="(value, key) in selectedNode.data" :key="key" v-if="selectedNode">
+      <label>{{ key }}</label>
+      <textarea v-if="typeof key === 'string' && key === 'description'" v-model="selectedNode.data[key]" class="description"></textarea>
+      <input v-else-if="typeof value === 'boolean'" type="checkbox" v-model="selectedNode.data[key]" />
+      <input v-else-if="value instanceof Date" type="date" v-model="selectedNode.data[key]" />
+      <input v-else v-model="selectedNode.data[key]" />
+    </div>
+    <button @click="selectedNodeId = null">Close</button>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -26,33 +42,46 @@ import { MiniMap, Background } from '@vue-flow/additional-components'
 import { Layout } from '@/components/layouts'
 import { useVueFlow } from '@vue-flow/core'
 import { VueFlow, type Elements } from '@vue-flow/core'
-import InputNode from './nodes/InputNode.vue'
-import OutputNode from './nodes/OutputNode.vue'
-import { ref, watch } from 'vue'
+import TacheNode from './nodes/TacheNode.vue'
+import JalonNode from './nodes/JalonNode.vue'
+import LivrableNode from './nodes/LivrableNode.vue'
+import { ref, computed } from 'vue'
 import '../css/roadap.css'
-import type { ElementData } from '@vue-flow/core'
 const { onConnect, addEdges } = useVueFlow()
+const selectedNodeId = ref(null)
+const selectedNode = computed(() => elements.value.find(node => node.id === selectedNodeId.value));
+
 onConnect((params) => {
   addEdges([params])
 })
 const elements = ref<Elements>([])
-var json = JSON.stringify(elements.value)
 
-const addNode = (type: string, text: String) => {
+var json = JSON.stringify(elements.value)
+const addNode = (type: string) => {
   const id = (elements.value.length + 1).toString() as unknown as string;
-  console.log(id);
+  // Calculate the x and y position for the new node
+  const lastNode = elements.value[elements.value.length - 1] as { position: { x: number, y: number } } | undefined;
+  const newX = lastNode ? lastNode.position.x + 20 : window.innerWidth / 2;
+  const newY = lastNode ? lastNode.position.y - 20 : window.innerHeight / 2;
+
   elements.value.push({
     type: type,
-    data: { label: `Node ${id}` },
+    data: { 
+      nom: `Nouveau `+ type,
+      description: `Description de `+ type,
+      durrée: 0,
+      commencer: true,
+     },
     position: {
-    x: window.innerWidth / 2,
-    y: window.innerHeight / 2,
+      x: newX ,
+      y: newY,
     },
-    label: text,
     animated: true,
-    id: id
-    });
+    id: id,
+    label: type,
+  });
 };
+
 const saveNode = () => {
   json = JSON.stringify(elements.value);
   console.log(json);
@@ -69,5 +98,60 @@ const importNode = () => {
   justify-content: space-between;
   padding: 10px;
   background-color: #f8f8f8;
+}
+
+.settingsWindow {
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 300px;
+  height: 100%;
+  background: #f8f8f8;
+  border-left: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+
+.settingsWindow h2 {
+  color: #333;
+  margin-bottom: 1rem;
+}
+
+.settingsWindow div {
+  width: 100%;
+  padding: 0.5rem;
+  background: #fff;
+  margin-bottom: 0.5rem;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+}
+
+.settingsWindow label {
+  display: block;
+  margin-bottom: 0.5rem;
+  color: #666;
+}
+
+.settingsWindow input {
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.settingsWindow button {
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  background: #333;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+.settingsWindow .description {
+  font-size: 0.8rem;
+  height: 4rem;
 }
 </style>
---
+++ Front/Pragmap/src/views/roadMap/nodes/LivrableNode.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { Handle, Position } from '@vue-flow/core'
+import { FileText } from 'lucide-vue-next';
+
+import { useNode } from '@vue-flow/core'
+
+// `useNode` returns us the node object straight from the state
+// since the node obj is reactive, we can mutate it to update our nodes' data
+const { node } = useNode()
+
+const emit = defineEmits(['nodeClicked']);
+</script>
+<template>
+  <div class="livrableNode" v-on:click="$emit('nodeClicked', node.id)">
+    <p>{{ node.data.nom }}</p>
+    <Handle type="source" :position="Position.Top"/>
+    <FileText />
+  </div>
+</template>
+
+<style scoped>
+.livrableNode {
+  height: 100%;
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background: white;
+  border: 1px solid green;
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+}
+</style>
