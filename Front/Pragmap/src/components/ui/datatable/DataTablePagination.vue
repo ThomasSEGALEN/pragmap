fd6ac347ab765720650d158e69575dd4fb735360
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-15 22:12:30 +0100

    Fix de la DataTablePagination

--- Front/Pragmap/src/components/ui/datatable/DataTablePagination.vue
+++ Front/Pragmap/src/components/ui/datatable/DataTablePagination.vue
@@ -29,7 +29,7 @@ const {
 	>
 		<div class="flex items-center space-x-2">
 			<div class="flex items-center justify-center text-sm font-medium">
-				Page {{ getPageIndex() + 1 }} sur {{ getPageCount() }}
+				Page {{ getPageIndex() + 1 }} sur {{ getPageCount() === 0 ? 1 : getPageCount() }}
 			</div>
 			<Button
 				variant="outline"
