b999195a4492cc19259c3682588193f88e15ad9e
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-15 22:15:57 +0100

    Ajout de la colonne Utilisateurs dans la liste des clients

--- Front/Pragmap/src/views/customers/columns.ts
+++ Front/Pragmap/src/views/customers/columns.ts
@@ -1,8 +1,11 @@
-import { h } from 'vue'
-import type { ColumnDef, Row } from '@tanstack/vue-table'
-import { useAuthStore } from '@/stores'
-import type { IGetCustomer } from '@/types'
+import { h, onMounted, ref } from 'vue'
+import type { ColumnDef } from '@tanstack/vue-table'
+import router from '@/router'
+import { customerService, userService } from '@/services'
+import type { ICustomerUser, IGetCustomer } from '@/types'
 import { DataTableColumnHeader, DataTableDropDown } from '@/components/ui/datatable'
+import { toast } from '@/components/ui/toast'
+import CustomerPopover from './CustomerPopover.vue'
 
 export const columns: Array<ColumnDef<IGetCustomer>> = [
 	{
@@ -10,6 +13,14 @@ export const columns: Array<ColumnDef<IGetCustomer>> = [
 		accessorKey: 'name',
 		header: ({ column }) => h(DataTableColumnHeader<IGetCustomer>, { column, title: 'Nom' }),
 		cell: ({ row }) => h('div', row.getValue('Nom'))
+	},
+	{
+		id: 'Utilisateurs',
+		accessorKey: 'customerUsers',
+		header: ({ column }) =>
+			h(DataTableColumnHeader<IGetCustomer>, { column, title: 'Utilisateurs' }),
+		cell: ({ row }) => h(CustomerPopover, { customerUsers: row.getValue('Utilisateurs') as Array<ICustomerUser> }),
+		enableSorting: false
 	},
 	{
 		id: 'Date de création',
@@ -23,7 +34,21 @@ export const columns: Array<ColumnDef<IGetCustomer>> = [
 		cell: ({ row }) =>
 			h(
 				'div',
-				h(DataTableDropDown, { name: 'CustomersEdit', id: row.original.id, email: row.original.name })
+				h(DataTableDropDown, {
+					name: 'CustomersEdit', id: row.original.id, deleteEntity: async (id: string) => {
+						try {
+							await customerService.delete(id)
+
+							router.go(0)
+						} catch (error) {
+							toast({
+								title: 'Erreur',
+								description: `Nous ne sommes pas parvenus à supprimer ce client.`,
+								duration: 5000
+							})
+						}
+					}
+				})
 			),
 		enableHiding: false
 	}
