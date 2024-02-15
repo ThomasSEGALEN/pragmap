4c815ebc0aaef4a9e6c92a0376a9d3f29e5c0711
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-15 22:14:13 +0100

    Export classique au lieu de default

--- Front/Pragmap/src/middlewares/auth.ts
+++ Front/Pragmap/src/middlewares/auth.ts
@@ -1,12 +1,10 @@
 import type { NavigationGuard } from 'vue-router'
 import { useAuthStore } from '@/stores'
 
-const authMiddleware: NavigationGuard = (to, from, next) => {
+export const authMiddleware: NavigationGuard = (to, from, next) => {
 	const { isAuthenticated } = useAuthStore()
 
 	if (!isAuthenticated) return next({ name: 'Login' })
 
 	return next()
 }
-
-export default authMiddleware
