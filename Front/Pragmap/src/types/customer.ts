5ba4129a8d0255f9d4d3aea4d653fc0603f0076b
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-22 23:57:25 +0100

    Modification des types Customer

--- Front/Pragmap/src/types/customer.ts
+++ Front/Pragmap/src/types/customer.ts
@@ -1,40 +1,30 @@
-import type { ICustomerUser } from "./customerUser"
+import type { ICustomerUser } from './customerUser'
 
 export interface ICustomer {
 	id: string
 	name: string
 	logo: string
-	userIds: Array<string>
+	userIds?: Array<string>
 	createdAt: string
 	updatedAt: string
-	customerUsers: Array<ICustomerUser>
+	customerUsers?: Array<ICustomerUser>
 }
 export interface IGetCustomer {
 	id: string
 	name: string
-	logo?: string
-	userIds: Array<string>
+	logo: string
+	userIds?: Array<string>
 	createdAt: string
-	updatedAt: string
+	customerUsers?: Array<ICustomerUser>
 }
 export interface IPostCustomer {
 	name: string
-	logo?: string
-	userIds: Array<string>
+	logo: string
+	userIds?: Array<string>
 }
 export interface IPutCustomer {
 	id: string
 	name: string
-	logo?: string
-	userIds: Array<string>
-	createdAt: string
-	updatedAt: string
-}
-export interface IDisplayCustomer {
-	id: string
-	name: string
-	logo?: string
-	userIds: Array<string>
-	createdAt: string
-	updatedAt: string
+	logo: string
+	userIds?: Array<string>
 }
