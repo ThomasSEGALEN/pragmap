a76e393b0e2a4d82eb87bc44c65d150d277e7941
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-22 23:51:59 +0100

    Réinstallation du composant Input

--- Front/Pragmap/src/components/ui/input/Input.vue
+++ Front/Pragmap/src/components/ui/input/Input.vue
@@ -4,13 +4,13 @@ import { useVModel } from '@vueuse/core'
 import { cn } from '@/lib/utils'
 
 const props = defineProps<{
-	defaultValue?: string | number | File
-	modelValue?: string | number | File
+	defaultValue?: string | number
+	modelValue?: string | number
 	class?: HTMLAttributes['class']
 }>()
 
 const emits = defineEmits<{
-	(e: 'update:modelValue', payload: string | number | File): void
+	(e: 'update:modelValue', payload: string | number): void
 }>()
 
 const modelValue = useVModel(props, 'modelValue', emits, {
@@ -25,7 +25,7 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 		:class="
 			cn(
 				'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-primary file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-				props.class ?? ''
+				props.class
 			)
 		"
 	/>
