5f2948758d1019b19fe8ecae9c1e0858e1f60113
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-26 00:13:06 +0100

    Mise à jour du composant Input pour afficher/cacher les champs textuels de type password

--- Front/Pragmap/src/components/ui/input/Input.vue
+++ Front/Pragmap/src/components/ui/input/Input.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
+import { ref, useAttrs, type HTMLAttributes, type InputTypeHTMLAttribute } from 'vue'
 import { useVModel } from '@vueuse/core'
 import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { Eye, EyeOff } from 'lucide-vue-next'
 
 const props = defineProps<{
 	defaultValue?: string | number
 	modelValue?: string | number
+	type?: InputTypeHTMLAttribute
 	class?: HTMLAttributes['class']
 }>()
 
@@ -17,16 +20,49 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 	passive: true,
 	defaultValue: props.defaultValue
 })
+
+const showPassword = ref(false)
+</script>
+
+<script lang="ts">
+export default {
+	inheritAttrs: false
+}
 </script>
 
 <template>
-	<input
-		v-model="modelValue"
-		:class="
-			cn(
-				'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-primary file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-				props.class
-			)
-		"
-	/>
+	<div class="relative">
+		<input
+			v-model="modelValue"
+			:id="useAttrs().id as string"
+			:name="useAttrs().name as string"
+			:type="showPassword ? 'text' : type"
+			:autocomplete="useAttrs().autocomplete as string"
+			:class="
+				cn(
+					'relative flex h-10 w-full rounded-md border border-input bg-background',
+					type === 'password' ? ' pl-3 pr-12 ' : ' px-3 ',
+					'py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-primary file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+					props.class
+				)
+			"
+		/>
+		<template v-if="type === 'password'">
+			<Button
+				class="absolute top-0 right-0 flex justify-center items-center"
+				type="button"
+				variant="link"
+				@click="showPassword = !showPassword"
+			>
+				<Eye
+					v-if="showPassword"
+					class="h-4 w-4"
+				/>
+				<EyeOff
+					v-else
+					class="h-4 w-4"
+				/>
+			</Button>
+		</template>
+	</div>
 </template>
