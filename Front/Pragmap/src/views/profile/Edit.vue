be18957e88c7f60b05b8f2d51e5226f03b71e644
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-25 16:52:42 +0100

    Création de la vue Edit du profil

---
+++ Front/Pragmap/src/views/profile/Edit.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores'
+
+const authStore = useAuthStore()
+console.log(authStore.user)
+</script>
+
+<template>
+	<div>Profile page</div>
+</template>
