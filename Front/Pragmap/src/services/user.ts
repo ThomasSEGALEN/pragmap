81a51d2270a837dc5a7d092b5f57a70dc4ff98f5
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-26 22:40:45 +0100

    Création askEmailUpdate dans le service User

--- Front/Pragmap/src/services/user.ts
+++ Front/Pragmap/src/services/user.ts
@@ -1,6 +1,18 @@
+import { api } from '@/main'
 import { BaseService } from '@/services'
 import type { IGetUser, IPostUser, IPutUser, IUser } from '@/types'
 
-class UserService extends BaseService<IUser, IGetUser, IPostUser, IPutUser> {}
+class UserService extends BaseService<IUser, IGetUser, IPostUser, IPutUser> {
+  public async askEmailUpdate(userId: string, email: string): Promise<void> {
+    try {
+      await api.post('/user/ask-email-update', {
+        userId: userId,
+        email: email
+      })
+    } catch (error) {
+      throw new Error('AskEmailUpdate Error')
+    }
+  }
+}
 
 export const userService = new UserService('user')
