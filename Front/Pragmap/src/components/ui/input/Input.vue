e7e0778827e99925b9dc58708b4b7aad6f315380
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-02-28 03:04:40 +0100

    Mise à jour du composant Input

--- Front/Pragmap/src/components/ui/input/Input.vue
+++ Front/Pragmap/src/components/ui/input/Input.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, useAttrs, type HTMLAttributes, type InputTypeHTMLAttribute } from 'vue'
+import { ref, useAttrs, type HTMLAttributes } from 'vue'
 import { useVModel } from '@vueuse/core'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
@@ -8,7 +8,6 @@ import { Eye, EyeOff } from 'lucide-vue-next'
 const props = defineProps<{
 	defaultValue?: string | number
 	modelValue?: string | number
-	type?: InputTypeHTMLAttribute
 	class?: HTMLAttributes['class']
 }>()
 
@@ -20,6 +19,8 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 	passive: true,
 	defaultValue: props.defaultValue
 })
+
+const inputType = useAttrs().type
 
 const showPassword = ref<boolean>(false)
 
@@ -37,25 +38,34 @@ export default {
 </script>
 
 <template>
-	<div class="relative">
+	<input
+		v-if="inputType !== 'password'"
+		v-bind="$attrs"
+		v-model="modelValue"
+		:class="
+			cn(
+				'relative flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-primary file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+				props.class
+			)
+		"
+	/>
+	<div
+		v-else
+		class="relative"
+	>
 		<input
+			v-bind="$attrs"
 			v-model="modelValue"
 			ref="refValue"
-			:id="useAttrs().id as string"
-			:name="useAttrs().name as string"
-			:type="showPassword ? 'text' : type"
-			:placeholder="useAttrs().placeholder as string"
-			:autocomplete="useAttrs().autocomplete as string"
+			:type="showPassword ? 'text' : inputType"
 			:class="
 				cn(
-					'relative flex h-10 w-full rounded-md border border-input bg-background',
-					type === 'password' ? ' pl-3 pr-12 ' : ' px-3 ',
-					'py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-primary file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+					'relative flex h-10 w-full rounded-md border border-input bg-background pl-3 pr-12 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-primary file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
 					props.class
 				)
 			"
 		/>
-		<template v-if="type === 'password'">
+		<template v-if="inputType === 'password'">
 			<Button
 				class="absolute top-0 right-0 flex justify-center items-center"
 				type="button"
