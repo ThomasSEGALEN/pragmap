9ba97347c3d55ca5c250bf7a45f72d16782f2274
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-03-13 22:32:29 +0100

    Mise à jour de la colonne de la DataTable User

--- Front/Pragmap/src/views/users/partials/columns.ts
+++ Front/Pragmap/src/views/users/partials/columns.ts
@@ -2,70 +2,68 @@ import { h } from 'vue'
 import type { ColumnDef, Row } from '@tanstack/vue-table'
 import router from '@/router'
 import { userService } from '@/services'
-import { useAuthStore } from '@/stores'
-import type { IGetUser } from '@/types'
+import { useAuthStore, type UsersData } from '@/stores'
 import { DataTableColumnHeader, DataTableDropDown } from '@/components/ui/datatable'
 import { toast } from '@/components/ui/toast'
 
 const { roles } = useAuthStore()
-const getRole = (row: Row<IGetUser>) => roles.find((role) => role.id === row.getValue('Rôle'))!.name
+const getRole = (row: Row<UsersData>) =>
+	roles.find((role) => role.id === row.getValue('role'))!.name
 
-export const columns: Array<ColumnDef<IGetUser>> = [
+export const columns: Array<ColumnDef<UsersData>> = [
 	{
-		id: 'Adresse e-mail',
+		id: 'email',
 		accessorKey: 'email',
-		header: ({ column }) => h(DataTableColumnHeader<IGetUser>, { column, title: 'Adresse e-mail' }),
-		cell: ({ row }) => h('div', row.getValue('Adresse e-mail'))
+		header: ({ column }) =>
+			h(DataTableColumnHeader<UsersData>, { column, title: 'Adresse e-mail' }),
+		cell: ({ row }) => h('div', row.getValue('email'))
 	},
 	{
-		id: 'Nom',
+		id: 'lastName',
 		accessorKey: 'lastName',
-		header: ({ column }) => h(DataTableColumnHeader<IGetUser>, { column, title: 'Nom' }),
-		cell: ({ row }) => h('div', row.getValue('Nom'))
+		header: ({ column }) => h(DataTableColumnHeader<UsersData>, { column, title: 'Nom' }),
+		cell: ({ row }) => h('div', row.getValue('lastName'))
 	},
 	{
-		id: 'Prénom',
+		id: 'firstName',
 		accessorKey: 'firstName',
-		header: ({ column }) => h(DataTableColumnHeader<IGetUser>, { column, title: 'Prénom' }),
-		cell: ({ row }) => h('div', row.getValue('Prénom'))
+		header: ({ column }) => h(DataTableColumnHeader<UsersData>, { column, title: 'Prénom' }),
+		cell: ({ row }) => h('div', row.getValue('firstName'))
 	},
 	{
-		id: 'Rôle',
+		id: 'role',
 		accessorKey: 'roleId',
-		header: ({ column }) => h(DataTableColumnHeader<IGetUser>, { column, title: 'Rôle' }),
+		header: ({ column }) => h(DataTableColumnHeader<UsersData>, { column, title: 'Rôle' }),
 		cell: ({ row }) => h('div', getRole(row)),
 		sortingFn: (rowA, rowB) => getRole(rowA).localeCompare(getRole(rowB))
 	},
 	{
-		id: 'Date de création',
+		id: 'createdAt',
 		accessorKey: 'createdAt',
 		header: ({ column }) =>
-			h(DataTableColumnHeader<IGetUser>, { column, title: 'Date de création' }),
-		cell: ({ row }) => h('div', new Date(row.getValue('Date de création')).toLocaleDateString())
+			h(DataTableColumnHeader<UsersData>, { column, title: 'Date de création' }),
+		cell: ({ row }) => h('div', new Date(row.getValue('createdAt')).toLocaleDateString())
 	},
 	{
-		id: 'Actions',
+		id: 'actions',
 		cell: ({ row }) =>
-			h(
-				'div',
-				h(DataTableDropDown, {
-					name: 'UsersEdit',
-					id: row.original.id,
-					deleteEntity: async (id: string) => {
-						try {
-							await userService.delete(id)
+			h(DataTableDropDown, {
+				name: 'UsersEdit',
+				id: row.original.id,
+				deleteEntity: async (id: string) => {
+					try {
+						await userService.delete(id)
 
-							router.go(0)
-						} catch (error) {
-							toast({
-								title: 'Erreur',
-								description: 'Nous ne sommes pas parvenus à supprimer cet utilisateur.',
-								duration: 5000
-							})
-						}
+						router.go(0)
+					} catch (error) {
+						toast({
+							title: 'Erreur',
+							description: 'Nous ne sommes pas parvenus à supprimer cet utilisateur.',
+							duration: 5000
+						})
 					}
-				})
-			),
+				}
+			}),
 		enableHiding: false
 	}
 ]
