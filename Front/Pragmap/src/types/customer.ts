24189eed495e8bbe5dcf0445c5917034c94195c1
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-03-13 22:09:45 +0100

    Modification des types

--- Front/Pragmap/src/types/customer.ts
+++ Front/Pragmap/src/types/customer.ts
@@ -4,27 +4,26 @@ export interface ICustomer {
 	id: string
 	name: string
 	logo: string
-	userIds?: Array<string>
+	userIds: Array<string | null>
 	createdAt: string
 	updatedAt: string
-	customerUsers?: Array<ICustomerUser>
+	customerUsers: Array<ICustomerUser> | null
 }
 export interface IGetCustomer {
 	id: string
 	name: string
 	logo: string
-	userIds?: Array<string>
 	createdAt: string
-	customerUsers?: Array<ICustomerUser>
+	customerUsers: Array<ICustomerUser> | null
 }
 export interface IPostCustomer {
 	name: string
 	logo: string
-	userIds?: Array<string>
+	userIds: Array<string | null>
 }
 export interface IPutCustomer {
 	id: string
 	name: string
 	logo: string
-	userIds?: Array<string>
+	userIds: Array<string | null>
 }
--- Front/Pragmap/src/types/roadmap.ts
+++ Front/Pragmap/src/types/roadmap.ts
@@ -1,27 +1,27 @@
 export interface IRoadmap {
 	id: string
 	name: string
-	data: string
+	data: Array<Record<any, any>> | null
+	customerId: string
+	customerName: string
+	createdAt: string
+	updatedAt: string
+}
+export interface IGetRoadmap {
+	id: string
+	name: string
+	data: Array<Record<any, any>> | null
 	customerId: string
 	createdAt: string
 	updatedAt: string
 }
-export interface IGetRoadMap {
-	id: string
+export interface IPostRoadmap {
 	name: string
-	data: string
-	customerId: string 
-	createdAt: string
-	updatedAt: string
-}
-export interface IPostRoadMap {
-	name: string
-	customerId: string 
+	customerId: string
 }
 
-export interface IPutRoadMap {
+export interface IPutRoadmap {
 	id: string
 	name: string
-	data?: string
-	customerId: string
+	data?: Array<Record<any, any>> | null
 }
