e97932eeaa51fd7a19905c782205dc5fc5fcf2dd
yonjukyu <yonjukyu2@gmail.com>
2024-03-18 23:59:24 +0100

    vincent fait pas grad chose quand meme je trouve nonobstant

--- Front/Pragmap/vite.config.ts
+++ Front/Pragmap/vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import inject from '@rollup/plugin-inject'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
@@ -14,5 +14,10 @@ export default defineConfig({
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
-	}
+	},
+	build: {
+        rollupOptions: {
+            plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })],
+        },
+    },
 })
