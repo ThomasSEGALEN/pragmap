a807fc4a78f1dc425e971cd1723bfba9a3c1b9e3
yonjukyu <yonjukyu2@gmail.com>
2024-03-19 00:10:15 +0100

    OYAWA

--- Front/Pragmap/vite.config.ts
+++ Front/Pragmap/vite.config.ts
@@ -15,9 +15,4 @@ export default defineConfig({
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
 	},
-	build: {
-        rollupOptions: {
-            plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })],
-        },
-    },
 })
