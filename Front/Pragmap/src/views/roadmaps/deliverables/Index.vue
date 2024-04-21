59e1d601220a0534fcc02560b10ab44f18828fa7
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-04-21 23:25:44 +0200

    Liste des livrables d'une roadmap

---
+++ Front/Pragmap/src/views/roadmaps/deliverables/Index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { getRoadmapData, type Node } from '@/stores'
+import { DataTable } from '@/components/ui/datatable'
+import { columns } from './partials/columns'
+
+const { id } = useRoute().params as { id: string }
+const data = ref<Array<Node>>([])
+
+data.value = await getRoadmapData(id, 'deliverable')
+</script>
+
+<template>
+	<DataTable
+		:data="data"
+		:columns="columns"
+	/>
+</template>
