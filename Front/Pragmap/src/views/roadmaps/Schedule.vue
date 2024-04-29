da2fb6df3331904e8c8e313a828cdde3268c4dea
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-04-29 22:04:11 +0200

    JSON bien stylax

--- Front/Pragmap/src/views/roadmaps/Schedule.vue
+++ Front/Pragmap/src/views/roadmaps/Schedule.vue
@@ -1,3 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { roadmapService } from '@/services'
 
-<template>Gantt</template>
+const { id } = useRoute().params as { id: string }
+
+const roadmapData = ref()
+
+onMounted(async () => (roadmapData.value = JSON.parse((await roadmapService.getById(id)).data)))
+</script>
+
+<template>
+	<pre>{{ roadmapData }}</pre>
+</template>
