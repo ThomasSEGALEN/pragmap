72bc89402303fd5c9102a90e8ab0911f5ec42380
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-06-06 23:34:27 +0200

    Gestion de la modification des tâches et livrables en fonction du rôle

--- Front/Pragmap/src/views/roadmaps/partials/UpdateDialog.vue
+++ Front/Pragmap/src/views/roadmaps/partials/UpdateDialog.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts" generic="TData">
 import { ref } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
 import { useFocus } from '@vueuse/core'
 import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import { z } from '@/lib/utils'
 import { roadmapService } from '@/services'
-import { useTableStore } from '@/stores'
+import { useAuthStore, useTableStore } from '@/stores'
+import { Role } from '@/types'
 import {
 	AlertDialog,
 	AlertDialogAction,
@@ -15,7 +16,6 @@ import {
 	AlertDialogDescription,
 	AlertDialogFooter,
 	AlertDialogHeader,
-	AlertDialogTitle,
 	AlertDialogTrigger
 } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
@@ -31,10 +31,17 @@ const { entities, entityId, entityData, entityType } = defineProps<{
 }>()
 
 const { id } = useRoute().params as { id: string }
-const router = useRouter()
+const { getRole } = useAuthStore()
+const role = (await getRole()).name
 const roadmapData = useTableStore().getData()
 const input = ref<HTMLInputElement | null>(null)
 useFocus(input, { initialValue: true })
+const labelTranslations: { [key: string]: string } = {
+	label: 'Label',
+	description: 'Description',
+	duration: 'Durée',
+	progress: 'Progression'
+}
 const formSchema = toTypedSchema(
 	z.object({
 		input: z
@@ -60,15 +67,17 @@ const onSubmit = handleSubmit(async (values) => {
 	const keys = update.key.split('.')
 	const finalKey = keys.pop() as string
 	const node = keys.reduce((node, key) => node[key], newEntity as { [key: string]: any })
+
 	node[finalKey] = update.value
+
 	const newData = roadmapData.map((obj: any) => (obj.id === entityId ? newEntity : obj))
 
 	try {
-		roadmapService.update(id, { id: id, data: JSON.stringify(newData) }).then(() => router.go(0))
+		roadmapService.update(id, { id: id, data: JSON.stringify(newData) })
 	} catch (error) {
 		toast({
 			title: 'Erreur',
-			description: `${values.input}`,
+			description: 'Nous ne sommes pas parvenus à modifier les données de cette roadmap.',
 			duration: 5000
 		})
 	}
@@ -76,7 +85,8 @@ const onSubmit = handleSubmit(async (values) => {
 </script>
 
 <template>
-	<AlertDialog>
+	<slot v-if="role === Role.Reader" />
+	<AlertDialog v-else>
 		<AlertDialogTrigger as-child>
 			<Button
 				class="px-2 focus-visible:ring-offset-0"
@@ -87,7 +97,6 @@ const onSubmit = handleSubmit(async (values) => {
 		</AlertDialogTrigger>
 		<AlertDialogContent>
 			<AlertDialogHeader>
-				<AlertDialogTitle>Modification des données : {{ entityData }}</AlertDialogTitle>
 				<AlertDialogDescription>
 					<form
 						class="space-y-6"
@@ -98,7 +107,7 @@ const onSubmit = handleSubmit(async (values) => {
 							name="input"
 						>
 							<FormItem>
-								<FormLabel>{{ entityData }}</FormLabel>
+								<FormLabel>{{ labelTranslations[entityData] }}</FormLabel>
 								<FormControl>
 									<Input
 										v-bind="componentField"
