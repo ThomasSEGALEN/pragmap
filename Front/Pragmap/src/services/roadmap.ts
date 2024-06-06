69b38b5a60a908ae1a90fc1696a8d431816ec4d2
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2024-06-06 23:30:04 +0200

    Modification de la méthode update pour le service Roadmap

--- Front/Pragmap/src/services/roadmap.ts
+++ Front/Pragmap/src/services/roadmap.ts
@@ -1,6 +1,23 @@
+import { api } from '@/main'
+import router from '@/router'
+import { sleep } from '@/lib/utils'
 import { BaseService } from '@/services'
 import type { IRoadmap, IGetRoadmap, IPostRoadmap, IPutRoadmap } from '@/types'
 
-class RoadmapService extends BaseService<IRoadmap, IGetRoadmap, IPostRoadmap, IPutRoadmap> {}
+class RoadmapService extends BaseService<IRoadmap, IGetRoadmap, IPostRoadmap, IPutRoadmap> {
+	public async update(id: string, data: IPutRoadmap): Promise<void> {
+		const currentRoute = router.currentRoute.value.name
+
+		await sleep()
+
+		try {
+			await api.put(`/${this.apiPath}/${id}`, data)
+		} catch (error) {
+			throw new Error('Update Error')
+		}
+
+		if (currentRoute === 'RoadmapsTasks') router.go(0)
+	}
+}
 
 export const roadmapService = new RoadmapService('roadmap')
